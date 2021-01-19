8e99014b0009620474c3548f7a3574d3d92af8fe
Daniel Shoichet <daniel.shoichet@gmail.com>
2021-01-19 00:47:00 -0600

    Cleaned up code into more distinct files

--- FaceFeatureDetection.py
+++ FaceFeatureDetection.py
@@ -88,6 +88,65 @@ def detect_marks(img, model, face):
     return marks
 
 
+def head_pose_points(img, rotation_vector, translation_vector, camera_matrix):
+    """
+    Get the points to estimate head pose sideways
+    Parameters
+    ----------
+    img : np.unit8
+        Original Image.
+    rotation_vector : Array of float64
+        Rotation Vector obtained from cv2.solvePnP
+    translation_vector : Array of float64
+        Translation Vector obtained from cv2.solvePnP
+    camera_matrix : Array of float64
+        The camera matrix
+    Returns
+    -------
+    (x, y) : tuple
+        Coordinates of line to estimate head pose
+    """
+    rear_size = 1
+    rear_depth = 0
+    front_size = img.shape[1]
+    front_depth = front_size * 2
+    val = [rear_size, rear_depth, front_size, front_depth]
+    point_2d = _get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
+    y = (point_2d[5] + point_2d[8]) // 2
+    x = point_2d[2]
+
+    return (x, y)
+
+
+def _get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val):
+    """Return the 3D points present as 2D for making annotation box"""
+    point_3d = []
+    dist_coeffs = np.zeros((4, 1))
+    rear_size = val[0]
+    rear_depth = val[1]
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+
+    front_size = val[2]
+    front_depth = val[3]
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d.append((-front_size, front_size, front_depth))
+    point_3d.append((front_size, front_size, front_depth))
+    point_3d.append((front_size, -front_size, front_depth))
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
+
+    # Map to 2d img points
+    (point_2d, _) = cv2.projectPoints(point_3d,
+                                      rotation_vector,
+                                      translation_vector,
+                                      camera_matrix,
+                                      dist_coeffs)
+    point_2d = np.int32(point_2d.reshape(-1, 2))
+    return point_2d
 
 
 def _get_square_box(box):
