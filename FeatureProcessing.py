264313a237c61566e772100225282b935076875d
addisonweatherhead <d4s-ZN2-uPS-6Qo>
2021-02-07 09:59:23 -0500

    Added open_mouth_detector() function to FeatureProcessing.py

--- FeatureProcessing.py
+++ FeatureProcessing.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+from face_detector import get_face_detector, find_faces
 
 def get_look_angles(nose, rotation_vector, translation_vector, camera_matrix, dist_coeffs, headpose_x1, headpose_x2):
     nose_end_point2D = get_nose_endpoint(rotation_vector, translation_vector, camera_matrix, dist_coeffs)
@@ -31,3 +32,46 @@ def get_nose_endpoint(rotation_vector, translation_vector, camera_matrix, dist_c
     (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
                                                      translation_vector, camera_matrix, dist_coeffs)
     return nose_end_point2D
+
+
+
+def open_mouth_detector(face_landmark_points) -> bool:
+    """ Returns true if the mouth detected in the image is open (or smiling with teeth)
+
+    :param image: np.ndarray
+    :return: bool
+
+    Example usage:
+
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    rects = find_faces(img, face_model)
+    if len(rects) > 1:
+        print("Multiple faces detected...")
+
+    shape = detect_marks(img, landmark_model, rects[0])
+    open_mouth_detector(shape)
+
+    Output: True
+    """
+
+    outer_points = [[49, 59], [50, 58], [51, 57], [52, 56], [53, 55]]
+    d_outer = [0] * 5
+    inner_points = [[61, 67], [62, 66], [63, 65]]
+    d_inner = [0] * 3
+
+
+
+    cnt_outer = 0
+    cnt_inner = 0
+    for i, (p1, p2) in enumerate(outer_points):
+        if d_outer[i] + 3 < face_landmark_points[p2][1] - face_landmark_points[p1][1]:
+            cnt_outer += 1
+    for i, (p1, p2) in enumerate(inner_points):
+        if d_inner[i] + 2 < face_landmark_points[p2][1] - face_landmark_points[p1][1]:
+            cnt_inner += 1
+    if cnt_outer > 2.5 and cnt_inner > 2:
+        print('Wombo works best if you save that smile for after :)')
+        return True
+    else:
+        return False
--- mouth_opening_detector.py
+++ mouth_opening_detector.py
@@ -9,7 +9,6 @@ from face_detector import get_face_detector, find_faces
 from face_landmarks import get_landmark_model, detect_marks, draw_marks
 
 
-
 def run_live():
     face_model = get_face_detector()
     landmark_model = get_landmark_model()
@@ -66,47 +65,3 @@ def run_live():
     cv2.destroyAllWindows()
 
 
-def open_mouth_detector(image) -> bool:
-    """ Returns true if the mouth detected in the image is open (or smiling with teeth)
-
-    :param image: np.ndarray
-    :return: bool
-
-    Example usage:
-    my_image = cv2.imread("person_with_mouth_open.jpg")
-    open_mouth_detector(my_image)
-
-    Output: True
-    """
-    face_model = get_face_detector()
-    landmark_model = get_landmark_model()
-    outer_points = [[49, 59], [50, 58], [51, 57], [52, 56], [53, 55]]
-    d_outer = [0] * 5
-    inner_points = [[61, 67], [62, 66], [63, 65]]
-    d_inner = [0] * 3
-
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    rects = find_faces(img, face_model)
-    if len(rects) > 1:
-        print("Multiple faces detected...")
-
-    shape = detect_marks(img, landmark_model, rects[0])
-
-    cnt_outer = 0
-    cnt_inner = 0
-    draw_marks(img, shape[48:])
-    for i, (p1, p2) in enumerate(outer_points):
-        if d_outer[i] + 3 < shape[p2][1] - shape[p1][1]:
-            cnt_outer += 1
-    for i, (p1, p2) in enumerate(inner_points):
-        if d_inner[i] + 2 < shape[p2][1] - shape[p1][1]:
-            cnt_inner += 1
-    if cnt_outer > 2.5 and cnt_inner > 2:
-        print('Wombo works best if you save that smile for after :)')
-        return True
-    else:
-        return False
-
-my_img = cv2.imread("utsav-womboai.jpg")
-print(open_mouth_detector(my_img))
